a99917b544c1e8183b441c24f72011dea2e4812b
Justin Godden <justingodden@gmail.com>
2023-01-24 20:35:27 +0000

    deat(display): basic update function

--- src/Display.cpp
+++ src/Display.cpp
@@ -2,12 +2,35 @@
 
 Display::Display()
 {
+    window = new sf::RenderWindow;
+    texture = new sf::Texture;
+    sprite = new sf::Sprite;
+    texture->create(width, height);
+
+    window->create(sf::VideoMode(width * scaleFactor, height * scaleFactor), "Game Boy Emulator");
 }
 
 Display::~Display()
 {
+    delete window;
+    delete texture;
+    delete sprite;
+    delete pixels;
 }
 
-void Display::update()
+void Display::update(std::array<uint8_t, 160 * 144 * 4> screenData)
 {
+    // incredibly inefficient. figure out how to type cast an array
+    for (uint i = 0; i < 160 * 144 * 4; i += 4)
+    {
+        pixels[i * 4] = screenData.at(i);
+        pixels[(i * 4) + 1] = screenData.at(i + 1);
+        pixels[(i * 4) + 2] = screenData.at(i + 2);
+        pixels[(i * 4) + 3] = screenData.at(i + 3);
+    }
+
+    texture->update(pixels);
+    sprite->setTexture(*texture);
+    sprite->setScale(scaleFactor, scaleFactor);
+    window->draw(*sprite);
 }
--- src/Display.h
+++ src/Display.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <array>
+#include <stdint.h>
+#include <SFML/Graphics.hpp>
+
 class Display
 {
 public:
@@ -7,5 +11,15 @@ public:
     ~Display();
 
 public:
-    void update();
+    void update(std::array<uint8_t, 160 * 144 * 4> screenData);
+
+private:
+    sf::RenderWindow *window = nullptr;
+    sf::Texture *texture = nullptr;
+    sf::Sprite *sprite = nullptr;
+    sf::Uint8 *pixels = new sf::Uint8[64 * 32 * 4];
+
+    uint8_t width = 160;
+    uint8_t height = 144;
+    uint8_t scaleFactor = 3;
 };
